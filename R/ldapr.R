d92a8d9d385bfb798e479a3f49b76e005420c4c2
Lukas KLass <lukas.klass@elringklinger.com>
2018-09-26 07:52:45 +0200

    ldapwhoami introduced

--- R/ldapr.R
+++ R/ldapr.R
@@ -35,3 +35,27 @@ ldapsearch <- function(...) {
     return(item)
   }))
 }
+
+#' Request LDAP ID
+#'
+#' This function serves as interface to the console function ldapwhoami
+#' It allows to bind to a LDAP server and receive the user id.
+#' The syntax of this function is based on the syntax of ldapwhoami
+#' \link{http://www.openldap.org/software/man.cgi?query=ldapwhoami&sektion=1&manpath=OpenLDAP+2.4-Release}
+#'
+#' @param ... Parameters for the call to ldapwhoami Each parameter must be named
+#' (e.g. h for the host definition) and may also be NULL for an parameter
+#' without value. Some values, like D must be double quoted for evaluation within
+#' quotes.
+#' @return User ID or NULL if the call wasn't successful.
+#'
+#' @details This package requires the library openldap to be installed (https://www.openldap.org/).
+#'
+#' @examples ldapwhoami(x = NULL, h = 'ldap.myhost.org', D = '"QueryUserName\@myDomain"', w = 'QueryUserPassword')
+#'
+#' @export
+ldapwhoami <- function(...) {
+  result = suppressWarnings(system(paste0('ldapwhoami ', paste0("-", names(c(...)), ifelse(lapply(c(...), is.null), "", paste0(" ", c(...))), collapse = " ")),
+                                   intern = T, wait = T))
+  return(if(length(result)) gsub("u\\:", "", result) else NULL)
+}
